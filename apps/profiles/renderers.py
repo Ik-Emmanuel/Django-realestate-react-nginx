82df6da720dae589473b5cbc9958c83edb2ee18d
Ikechukwu Emmanuel <nwokochaikechukwu13@gmail.com>
2022-06-12 23:06:23 +0100

    minor-fix: profile renderers documentation

--- apps/profiles/renderers.py
+++ apps/profiles/renderers.py
@@ -11,3 +11,31 @@ class ProfileJSONRenderer(JSONRenderer):
             return super(ProfileJSONRenderer, self).render(data)
 
         return json.dumps({"profile": data})
+
+
+"""
+Allows response to come as 
+        {
+	"profile": {
+		"username": "obicubana",
+		"first_name": "Obi",
+		"last_name": "Cubana",
+		"full_name": "Obi Cubana",
+		"email": "obicubana@gmail.com",
+		"id": "30af3aeb-12bc-4c60-90de-a06a1267f1d6",
+		"phone_number": "+2349053234262",
+		"profile_photo": "http://localhost:8000/mediafiles/profile_default.png",
+		"about_me": "Magnate of Cubana empire",
+		"license": null,
+		"gender": "Male",
+		"country": "Nigeria",
+		"city": "Abuja",
+		"is_buyer": false,
+		"is_seller": false,
+		"is_agent": true,
+		"rating": null,
+		"num_reviews": 0,
+		"reviews": [],
+		"top_agent": true
+	}
+}"""
