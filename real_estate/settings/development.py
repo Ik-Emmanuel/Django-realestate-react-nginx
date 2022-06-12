19ba4baa71ac90e1a82deb8ec7aa57762b70158f
Ikechukwu Emmanuel <nwokochaikechukwu13@gmail.com>
2022-06-12 13:26:10 +0100

    chore: splitting settings.py

---
+++ real_estate/settings/development.py
@@ -0,0 +1,22 @@
+from .base import *
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": env("POSTGRES_ENGINE"),
+        "NAME": env("POSTGRES_DB"),
+        "USER": env("POSTGRES_USER"),
+        "PASSWORD": env("POSTGRES_PASSWORD"),
+        "HOST": env("POSTGRES_HOST"),
+        "PORT": env("POSTGRES_PORT"),
+    }
+}
+
+
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
