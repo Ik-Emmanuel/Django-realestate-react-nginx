4abd7fbc745f5f54c07596f608d70961fc8819a6
Ikechukwu Emmanuel <nwokochaikechukwu13@gmail.com>
2022-06-14 10:20:08 +0100

    chore: add react frontend service

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+  
+  <h1> My  Nuspace RealEstate </h1>
+  <p>The site starts here </p>
+
+
+    </div>
+  );
+}
+
+export default App;
